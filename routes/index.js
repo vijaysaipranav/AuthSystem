94d259bda07b3eab684161be07b468eb96555d88
vijaysaipranav <vijaysaiprant@gmail.com>
2023-07-19 23:09:41 +0530

    new update

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 console.log('router loaded');
 // implementing the routes of users and reset password
+router.use('/',async(req,res)=>{
+    res.redirect('/users/sign-up');
+})
 router.use('/users',require('./user'));
 router.use('/reset-password',require('./resetPass'));
 
